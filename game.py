66be6ee0d55a48538efae9edc2372c90181c07a7
nevess-g <guigobatista.28@gmail.com>
2026-05-16 21:24:38 -0300

    documentação dos movimentos e criação do arquivo para o menu

--- game.py
+++ game.py
@@ -10,23 +10,38 @@ janela = pygame.display.set_mode((WIDTH, HEIGHT))
 clock = pygame.time.Clock()
 FPS = 60
 
+# estrutura de dado para o andamento do loop
 game = True
 
+# agrupa todos os sprites
 all_sprites = pygame.sprite.Group()
+
+# instancia a classe Rapsoa
 raposa = Raposa()
+
+# adiciona à lista de sprites o sprite atualizado da raposa
 all_sprites.add(raposa)
 
 while game:
+    # define a velocidade do jogo
     clock.tick(FPS)
+
+    # preenche a janela com preto
     janela.fill(PRETO)
 
+    # itera sobre todos os eventos do jogo
     for event in pygame.event.get():
         if event.type == pygame.QUIT:
             game = False
 
+    # verifica as teclas pressionadas 
     keys = pygame.key.get_pressed()
+
+    # zera as velocidades X e Y da raposa
     raposa.velocity_x = 0
     raposa.velocity_y = 0
+
+    # altera a velocidade da raposa de acordo com a direção pressionada
     if keys[pygame.K_a]:
         raposa.velocity_x = -raposa.speed
     elif keys[pygame.K_d]:
@@ -36,6 +51,7 @@ while game:
     elif keys[pygame.K_s]:
         raposa.velocity_y = raposa.speed
 
+    # atualiza os sprites e mostra as animações na tela
     all_sprites.update()
     all_sprites.draw(janela)
     pygame.display.update()
